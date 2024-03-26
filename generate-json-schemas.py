70da4cdd9fe0ecb2f28f4d9ae1881831c9cf9ff6
Guillaume Charbonnier <guillaume.charbonnier@araymond.com>
2024-03-26 20:39:38 +0100

    refactor schema generation

--- generate-json-schemas.py
+++ generate-json-schemas.py
@@ -14,47 +14,63 @@ OUTPUT = "schemas"
 SOURCES = Path(__file__).parent.joinpath("src/schemas")
 
 
-def replace_ref(schema: dict[str, Any], prefix: str) -> None:
-    ref: str | None = schema.pop("$ref", None)
-    id: str | None = schema.pop("$id", None)
-    if ref:
-        ref = ref.split("/")[0]
-        schema["$ref"] = f"{prefix}/{ref}"
-    if id:
-        schema["$id"] = f"{prefix}/{id}"
+def replace_refs_and_ids(schema: dict[str, Any], ref_prefix: str) -> None:
+    if "$ref" in schema:
+        ref_template: str = schema["$ref"]
+        ref_value = ref_template.split("/")[0]
+        schema["$ref"] = f"{ref_prefix}/{ref_value}"
+    if "$id" in schema:
+        id_value = schema["$id"]
+        schema["$id"] = f"{ref_prefix}/{id_value}"
     for value in schema.values():
         if isinstance(value, dict):
-            replace_ref(value, prefix)
+            replace_refs_and_ids(value, ref_prefix)
         elif isinstance(value, list):
             for item in value:
                 if isinstance(item, dict):
-                    replace_ref(item, prefix)
+                    replace_refs_and_ids(item, ref_prefix)
 
 
-def create_schema(source: Path, prefix: str, output: str) -> None:
-    destination = Path(output).joinpath(source.name)
+def create_schema(source: Path, output_directory: Path, ref_prefix: str) -> None:
+    destination = output_directory.joinpath(source.name)
     content = json.loads(source.read_text())
-    replace_ref(content, f"{prefix}/{output}")
+    replace_refs_and_ids(content, ref_prefix)
     destination.write_text(json.dumps(content, indent=2))
 
 
-def main(repository: str, reference: str, output: str):
-    # Normalize repository and reference
+def main(repository: str, reference: str, output_directory: str):
+    # Validate reference and output
+    if not (repository and reference and output_directory):
+        raise ValueError("repository, reference and output_directory are required")
+    if output_directory.startswith("/"):
+        raise ValueError("output must be relative")
     if reference.startswith("/"):
-        reference = reference[1:]
+        raise ValueError("reference cannot start with '/'")
+    # Clean output directory
+    destination = Path(output_directory)
+    shutil.rmtree(destination, ignore_errors=True)
+    destination.mkdir(parents=False, exist_ok=True)
+    # Normalize repository, reference and output
+    if not reference.endswith("/"):
+        reference += "/"
     if not repository.endswith("/"):
         repository += "/"
-    repository = urljoin(repository, reference)
-    shutil.rmtree(output, ignore_errors=True)
-    Path(output).mkdir(parents=False, exist_ok=True)
+    if output_directory.endswith("/"):
+        output_directory = output_directory[:-1]
+    # Build full repository URL
+    repository_url = repository
+    for additional_prefix in (reference, output_directory):
+        repository_url = urljoin(repository_url, additional_prefix)
     # Generate all schemas
     for source in SOURCES.glob("*.json"):
-        create_schema(source, repository, output)
+        create_schema(
+            source=source, output_directory=destination, ref_prefix=repository_url
+        )
 
 
 if __name__ == "__main__":
     main(
         repository=environ.get("REPOSITORY", REPOSITORY),
         reference=environ.get("REFERENCE", REFERENCE),
-        output=environ.get("OUTPUT", OUTPUT),
+        output_directory=environ.get("OUTPUT", OUTPUT),
     )
