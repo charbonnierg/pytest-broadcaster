f1e8ac6bb9a28ed3240a1c164975b5093170a406
Guillaume Charbonnier <guillaume.charbonnier@araymond.com>
2024-03-26 20:21:11 +0100

    change schema generation strategy

--- generate-json-schemas.py
+++ generate-json-schemas.py
@@ -3,70 +3,58 @@ from __future__ import annotations
 
 import json
 import shutil
+from os import environ
 from pathlib import Path
-from sys import exit
 from typing import Any
+from urllib.parse import urljoin
 
+REPOSITORY = "https://raw.githubusercontent.com/charbonnierg/pytest-discover"
+REFERENCE = "main"
+OUTPUT = "schemas"
 SOURCES = Path(__file__).parent.joinpath("src/schemas")
-DESTINATIONS = Path(__file__).parent.joinpath("schemas")
 
 
-def replace_ref(schema: dict[str, Any], top: bool) -> None:
+def replace_ref(schema: dict[str, Any], prefix: str) -> None:
     ref: str | None = schema.pop("$ref", None)
-    if not top:
-        schema.pop("$schema", None)
-        schema.pop("$id", None)
+    id: str | None = schema.pop("$id", None)
     if ref:
         ref = ref.split("/")[0]
-        ref_path = DESTINATIONS.joinpath(ref)
-        ref_content = json.loads(ref_path.read_text())
-        replace_ref(ref_content, False)
-        schema.update(ref_content)
+        schema["$ref"] = f"{prefix}/{ref}"
+    if id:
+        schema["$id"] = f"{prefix}/{id}"
     for value in schema.values():
         if isinstance(value, dict):
-            replace_ref(value, False)
+            replace_ref(value, prefix)
         elif isinstance(value, list):
             for item in value:
                 if isinstance(item, dict):
-                    replace_ref(item, False)
+                    replace_ref(item, prefix)
 
 
-def create_schema(source: Path) -> None:
-    destination = DESTINATIONS.joinpath(source.name)
+def create_schema(source: Path, prefix: str, output: str) -> None:
+    destination = Path(output).joinpath(source.name)
     content = json.loads(source.read_text())
-    replace_ref(content, True)
-    output = json.dumps(content, indent=2)
-    destination.write_text(output)
+    replace_ref(content, f"{prefix}/{output}")
+    destination.write_text(json.dumps(content, indent=2))
 
 
-def main(max_passes: int):
-    shutil.rmtree(DESTINATIONS, ignore_errors=True)
-    DESTINATIONS.mkdir(parents=False, exist_ok=True)
-    max_passes = max(max_passes - 1, 1)
-    retry: list[Path] = []
-    # Generate all schemas without references
+def main(repository: str, reference: str, output: str):
+    # Normalize repository and reference
+    if reference.startswith("/"):
+        reference = reference[1:]
+    if not repository.endswith("/"):
+        repository += "/"
+    repository = urljoin(repository, reference)
+    shutil.rmtree(output, ignore_errors=True)
+    Path(output).mkdir(parents=False, exist_ok=True)
+    # Generate all schemas
     for source in SOURCES.glob("*.json"):
-        try:
-            create_schema(source)
-        except FileNotFoundError:
-            retry.append(source)
-            continue
-    # Attempt to generate remaining schemas until max_passes
-    for _ in range(max_passes - 1):
-        while retry:
-            source = retry.pop()
-            try:
-                create_schema(source)
-            except FileNotFoundError:
-                retry.append(source)
-
-    if retry:
-        print("Failed to generate schemas for:")
-        for source in retry:
-            print(f"  {source}")
-        exit(1)
+        create_schema(source, repository, output)
 
 
 if __name__ == "__main__":
-    # Allow at most 3 passes to generate schemas
-    main(max_passes=3)
+    main(
+        repository=environ.get("REPOSITORY", REPOSITORY),
+        reference=environ.get("REFERENCE", REFERENCE),
+        output=environ.get("OUTPUT", OUTPUT),
+    )
