604c41b37658b839fa714f627d0ddf3d5cadba20
Guillaume Charbonnier <guillaume.charbonnier@araymond.com>
2024-03-29 02:25:06 +0100

    refactor markers filter

--- demo-project/src/components/pytest-discover/markers-filters/MarkersFilters.tsx
+++ demo-project/src/components/pytest-discover/markers-filters/MarkersFilters.tsx
@@ -4,16 +4,17 @@ import SlTag from "@shoelace-style/shoelace/dist/react/tag/index.js"
 import type { IncludedExcludeStatus } from "../../../lib/filter"
 import "./MarkersFilters.css"
 
-export const MarkersFilters = ({
-  choices,
-  get,
-  onClick,
-}: {
+interface MarkersFiltersProps {
   choices: string[]
   get: (marker: string) => IncludedExcludeStatus
   onClick: (marker: string) => void
-}) => {
-  const variant = (marker: string) => {
+}
+
+const variantFactory =
+  (get: (marker: string) => IncludedExcludeStatus) =>
+  (
+    marker: string,
+  ): "primary" | "success" | "neutral" | "warning" | "danger" | "text" => {
     const status = get(marker)
     if (status === "included") {
       return "success"
@@ -23,6 +24,9 @@ export const MarkersFilters = ({
     }
     return "neutral"
   }
+
+export const MarkersFilters = ({ choices, get, onClick }: MarkersFiltersProps) => {
+  const variant = variantFactory(get)
   return (
     <div className="tags-selection">
       {Array.from(choices).map((marker) => {
