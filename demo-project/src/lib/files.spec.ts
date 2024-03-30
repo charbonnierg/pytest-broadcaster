dfae8d026c8dc41d5b6c0653256194eeb99a5f34
Guillaume Charbonnier <guillaume.charbonnier@araymond.com>
2024-03-30 03:10:11 +0100

    format

--- demo-project/src/lib/files.spec.ts
+++ demo-project/src/lib/files.spec.ts
@@ -113,7 +113,7 @@ describe.concurrent("Tree", () => {
       ])
     })
   })
-  describe.concurrent("simple tree with multiple items", () => {
+  describe.concurrent("tree with multiple items", () => {
     it("should return true when item is included", () => {
       const tree = makeTree()
       const item1 = makeExample()
@@ -193,7 +193,7 @@ describe.concurrent("Tree", () => {
       ])
     })
   })
-  describe.concurrent("complex tree wth several suites", () => {
+  describe.concurrent("tree wth several suites", () => {
     it("should return true when item is included", () => {
       const tree = makeTree()
       const item1 = makeExample()
@@ -324,162 +324,151 @@ describe.concurrent("Tree", () => {
       ])
     })
   })
-  describe.concurrent(
-    "complex tree with several suites and nested suites",
-    () => {
-      it("should return true when item is included", () => {
-        const tree = makeTree()
-        const item1 = makeExample()
-        const item2 = evolve(item1, { name: "test_another", parent: "suite1" })
-        const item3 = evolve(item1, {
-          name: "test_yet_another",
-          parent: "suite2::suite3",
-        })
-        tree.add(item1)
-        tree.add(item2)
-        tree.add(item3)
-        expect(tree.includes(item1.node_id)).toBe(true)
-        expect(tree.includes(item2.node_id)).toBe(true)
-        expect(tree.includes(item3.node_id)).toBe(true)
+  describe.concurrent("tree with nested suites", () => {
+    it("should return true when item is included", () => {
+      const tree = makeTree()
+      const item1 = makeExample()
+      const item2 = evolve(item1, { name: "test_another", parent: "suite1" })
+      const item3 = evolve(item1, {
+        name: "test_yet_another",
+        parent: "suite2::suite3",
       })
-      it("should return true when item is included according to prefix", () => {
-        const tree = makeTree()
-        const item1 = makeExample()
-        const item2 = evolve(item1, { name: "test_another", parent: "suite1" })
-        const item3 = evolve(item1, {
-          name: "test_yet_another",
-          parent: "suite2::suite3",
-        })
-        tree.add(item1)
-        tree.add(item2)
-        tree.add(item3)
-        expect(tree.includes(item1.node_id, "parent")).toBe(true)
-        expect(tree.includes(item2.node_id, "parent")).toBe(true)
-        expect(tree.includes(item3.node_id, "parent")).toBe(true)
-        expect(tree.includes(item2.node_id, "parent/test.py::suite1")).toBe(
-          true,
-        )
-        expect(tree.includes(item3.node_id, "parent/test.py::suite2")).toBe(
-          true,
-        )
-        expect(
-          tree.includes(item3.node_id, "parent/test.py::suite2::suite3"),
-        ).toBe(true)
+      tree.add(item1)
+      tree.add(item2)
+      tree.add(item3)
+      expect(tree.includes(item1.node_id)).toBe(true)
+      expect(tree.includes(item2.node_id)).toBe(true)
+      expect(tree.includes(item3.node_id)).toBe(true)
+    })
+    it("should return true when item is included according to prefix", () => {
+      const tree = makeTree()
+      const item1 = makeExample()
+      const item2 = evolve(item1, { name: "test_another", parent: "suite1" })
+      const item3 = evolve(item1, {
+        name: "test_yet_another",
+        parent: "suite2::suite3",
       })
-      it("should return false when item is not included according to prefix", () => {
-        const tree = makeTree()
-        const item1 = makeExample()
-        const item2 = evolve(item1, { name: "test_another", parent: "suite1" })
-        const item3 = evolve(item1, {
-          name: "test_yet_another",
-          parent: "suite2::suite3",
-        })
-        tree.add(item1)
-        tree.add(item2)
-        tree.add(item3)
-        expect(tree.includes(item1.node_id, "parent")).toBe(true)
-        expect(tree.includes(item2.node_id, "parent")).toBe(true)
-        expect(tree.includes(item3.node_id, "parent")).toBe(true)
-        expect(tree.includes(item3.node_id, "parent/test.py::suite1")).toBe(
-          false,
-        )
-        expect(tree.includes(item2.node_id, "parent/test.py::suite2")).toBe(
-          false,
-        )
-        expect(
-          tree.includes(item3.node_id, "parent/test.py::suite2::suite4"),
-        ).toBe(false)
+      tree.add(item1)
+      tree.add(item2)
+      tree.add(item3)
+      expect(tree.includes(item1.node_id, "parent")).toBe(true)
+      expect(tree.includes(item2.node_id, "parent")).toBe(true)
+      expect(tree.includes(item3.node_id, "parent")).toBe(true)
+      expect(tree.includes(item2.node_id, "parent/test.py::suite1")).toBe(true)
+      expect(tree.includes(item3.node_id, "parent/test.py::suite2")).toBe(true)
+      expect(
+        tree.includes(item3.node_id, "parent/test.py::suite2::suite3"),
+      ).toBe(true)
+    })
+    it("should return false when item is not included according to prefix", () => {
+      const tree = makeTree()
+      const item1 = makeExample()
+      const item2 = evolve(item1, { name: "test_another", parent: "suite1" })
+      const item3 = evolve(item1, {
+        name: "test_yet_another",
+        parent: "suite2::suite3",
       })
-      it("should return the list of names", () => {
-        const tree = makeTree()
-        const item1 = makeExample()
-        const item2 = evolve(item1, { name: "test_another", parent: "suite1" })
-        const item3 = evolve(item1, {
-          name: "test_yet_another",
-          parent: "suite2::suite3",
-        })
-        tree.add(item1)
-        tree.add(item2)
-        tree.add(item3)
-        expect(tree.list().map((item) => item.path)).toStrictEqual([
-          "parent",
-          "parent/test.py",
-          "parent/test.py::test_something",
-          "parent/test.py::suite1",
-          "parent/test.py::suite1::test_another",
-          "parent/test.py::suite2",
-          "parent/test.py::suite2::suite3",
-          "parent/test.py::suite2::suite3::test_yet_another",
-        ])
+      tree.add(item1)
+      tree.add(item2)
+      tree.add(item3)
+      expect(tree.includes(item1.node_id, "parent")).toBe(true)
+      expect(tree.includes(item2.node_id, "parent")).toBe(true)
+      expect(tree.includes(item3.node_id, "parent")).toBe(true)
+      expect(tree.includes(item3.node_id, "parent/test.py::suite1")).toBe(false)
+      expect(tree.includes(item2.node_id, "parent/test.py::suite2")).toBe(false)
+      expect(
+        tree.includes(item3.node_id, "parent/test.py::suite2::suite4"),
+      ).toBe(false)
+    })
+    it("should return the list of names", () => {
+      const tree = makeTree()
+      const item1 = makeExample()
+      const item2 = evolve(item1, { name: "test_another", parent: "suite1" })
+      const item3 = evolve(item1, {
+        name: "test_yet_another",
+        parent: "suite2::suite3",
       })
-      it("should return the list of nodes", () => {
-        const tree = makeTree()
-        const item1 = makeExample()
-        const item2 = evolve(item1, { name: "test_another", parent: "suite1" })
-        const item3 = evolve(item1, {
-          name: "test_yet_another",
-          parent: "suite2::suite3",
-        })
-        tree.add(item1)
-        tree.add(item2)
-        tree.add(item3)
-        expect(tree.list()).toStrictEqual([
-          {
-            name: "parent",
-            path: "parent",
-            type: NodeType.Directory,
-            parent: undefined,
-          },
-          {
-            name: "test.py",
-            path: "parent/test.py",
-            type: NodeType.File,
-            parent: "parent",
-          },
-          {
-            name: "test_something",
-            path: "parent/test.py::test_something",
-            type: NodeType.Case,
-            parent: "parent/test.py",
-            properties: item1,
-          },
-          {
-            path: "parent/test.py::suite1",
-            type: NodeType.Suite,
-            parent: "parent/test.py",
-            name: "suite1",
-          },
-          {
-            name: "test_another",
-            path: "parent/test.py::suite1::test_another",
-            type: NodeType.Case,
-            parent: "parent/test.py::suite1",
-            properties: item2,
-          },
-          {
-            path: "parent/test.py::suite2",
-            type: NodeType.Suite,
-            parent: "parent/test.py",
-            name: "suite2",
-          },
-          {
-            path: "parent/test.py::suite2::suite3",
-            type: NodeType.Suite,
-            parent: "parent/test.py::suite2",
-            name: "suite3",
-          },
-          {
-            name: "test_yet_another",
-            path: "parent/test.py::suite2::suite3::test_yet_another",
-            type: NodeType.Case,
-            parent: "parent/test.py::suite2::suite3",
-            properties: item3,
-          },
-        ])
+      tree.add(item1)
+      tree.add(item2)
+      tree.add(item3)
+      expect(tree.list().map((item) => item.path)).toStrictEqual([
+        "parent",
+        "parent/test.py",
+        "parent/test.py::test_something",
+        "parent/test.py::suite1",
+        "parent/test.py::suite1::test_another",
+        "parent/test.py::suite2",
+        "parent/test.py::suite2::suite3",
+        "parent/test.py::suite2::suite3::test_yet_another",
+      ])
+    })
+    it("should return the list of nodes", () => {
+      const tree = makeTree()
+      const item1 = makeExample()
+      const item2 = evolve(item1, { name: "test_another", parent: "suite1" })
+      const item3 = evolve(item1, {
+        name: "test_yet_another",
+        parent: "suite2::suite3",
       })
-    },
-  )
-  describe.concurrent("simple tree with parametrization and suite", () => {
+      tree.add(item1)
+      tree.add(item2)
+      tree.add(item3)
+      expect(tree.list()).toStrictEqual([
+        {
+          name: "parent",
+          path: "parent",
+          type: NodeType.Directory,
+          parent: undefined,
+        },
+        {
+          name: "test.py",
+          path: "parent/test.py",
+          type: NodeType.File,
+          parent: "parent",
+        },
+        {
+          name: "test_something",
+          path: "parent/test.py::test_something",
+          type: NodeType.Case,
+          parent: "parent/test.py",
+          properties: item1,
+        },
+        {
+          path: "parent/test.py::suite1",
+          type: NodeType.Suite,
+          parent: "parent/test.py",
+          name: "suite1",
+        },
+        {
+          name: "test_another",
+          path: "parent/test.py::suite1::test_another",
+          type: NodeType.Case,
+          parent: "parent/test.py::suite1",
+          properties: item2,
+        },
+        {
+          path: "parent/test.py::suite2",
+          type: NodeType.Suite,
+          parent: "parent/test.py",
+          name: "suite2",
+        },
+        {
+          path: "parent/test.py::suite2::suite3",
+          type: NodeType.Suite,
+          parent: "parent/test.py::suite2",
+          name: "suite3",
+        },
+        {
+          name: "test_yet_another",
+          path: "parent/test.py::suite2::suite3::test_yet_another",
+          type: NodeType.Case,
+          parent: "parent/test.py::suite2::suite3",
+          properties: item3,
+        },
+      ])
+    })
+  })
+  describe.concurrent("tree with parametrization within suite", () => {
     it("should return nodes", () => {
       const tree = makeTree()
       const item = evolve(makeExample(), {
