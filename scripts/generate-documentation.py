7f8a547c65d547e7d070b0456fd45a27262f3b20
Guillaume Charbonnier <guillaume.charbonnier@araymond.com>
2024-04-05 16:13:07 +0200

    fixed JSON schemas

--- scripts/generate-documentation.py
+++ scripts/generate-documentation.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import json
+import os
 import shutil
 from os import environ
 from pathlib import Path
@@ -11,19 +12,21 @@ from urllib.parse import urljoin
 
 import mkdocs_gen_files.nav
 
-REPOSITORY = "https://charbonnierg.github.com/pytest-broadcaster"
+REPOSITORY = "https://charbonnierg.github.io/pytest-broadcaster"
 
 SOURCES = Path(__file__).parent.parent.joinpath("src/schemas")
+
+VERSION = os.environ.get("VERSION", "latest")
 
 
 def replace_refs_and_ids(schema: dict[str, Any], ref_prefix: str) -> None:
     if "$ref" in schema:
         ref_template: str = schema["$ref"]
         ref_value = ref_template.split("/")[0]
-        schema["$ref"] = f"{ref_prefix}/{ref_value}"
+        schema["$ref"] = f"{ref_prefix}/{VERSION}/{ref_value}"
     if "$id" in schema:
         id_value = schema["$id"]
-        schema["$id"] = f"{ref_prefix}/{id_value}"
+        schema["$id"] = f"{ref_prefix}/{VERSION}/{id_value}"
     for value in schema.values():
         if isinstance(value, dict):
             replace_refs_and_ids(value, ref_prefix)
